aa6546da7b8766cf4a4c679ec426fe4d812a7125
diegu <89485216+jdiegu@users.noreply.github.com>
2023-03-22 14:30:20 -0600

    Delete Actividades2.1 directory

--- Actividades2.1/AlgebraLineal/src/sEcuaciones/Main.java
+++
@@ -1,9 +0,0 @@
-package sEcuaciones;
-
-public class Main {
-	public static void main(String args[]) {
-		Administra tester = new Administra();
-		
-		tester.solucion();
-	}
-}
--- Actividades2.1/Composicion3/src/composicion3/Main.java
+++
@@ -1,8 +0,0 @@
-package composicion3;
-
-public class Main {
-	public static void main(String args[]) {
-		Composicion tester = new Composicion();
-		tester.crea();
-	}
-}
